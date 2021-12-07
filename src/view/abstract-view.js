09258b1e80b682193c9078dc2743e9ea7883a46a
vpalex999 <vpalex@mail.ru>
2021-12-07 20:41:51 +0500

    move common methods to abstract class

--- src/view/abstract-view.js
+++ src/view/abstract-view.js
@@ -1,8 +1,28 @@
+import { createElement } from './render';
+
 class Abstract {
+  #element = null;
+
   constructor() {
     if (new.target === Abstract) {
       throw new Error('Can\'t instantiate AbstractView, only concrete one.');
     }
+  }
+
+  get template() {
+    throw new Error('Absctract method not implemented: get template');
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  removeElement() {
+    this.#element = null;
   }
 }
 
