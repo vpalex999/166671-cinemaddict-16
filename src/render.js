ec9d77e5bf114f7388dfe1e63fd0201a17a8d4cb
vpalex999 <vpalex@mail.ru>
2021-12-09 07:51:04 +0500

    refactoring renderElement

--- src/render.js
+++ src/render.js
@@ -1,3 +1,5 @@
+import { AbstractView } from './view/abstract-view';
+
 const RenderPosition = {
   BEFOREBEGIN: 'beforebegin',
   AFTERBEGIN: 'afterbegin',
@@ -6,21 +8,22 @@ const RenderPosition = {
 };
 
 const renderElement = (container, element, place) => {
-  if (element) {
-    switch (place) {
-      case RenderPosition.BEFOREBEGIN:
-        container.before(element);
-        break;
-      case RenderPosition.AFTERBEGIN:
-        container.prepend(element);
-        break;
-      case RenderPosition.BEFOREEND:
-        container.append(element);
-        break;
-      case RenderPosition.AFTEREND:
-        container.after(element);
-        break;
-    }
+  const parent = container instanceof AbstractView ? container.element : container;
+  const child = element instanceof AbstractView ? element.element : element;
+
+  switch (place) {
+    case RenderPosition.BEFOREBEGIN:
+      parent.before(child);
+      break;
+    case RenderPosition.AFTERBEGIN:
+      parent.prepend(child);
+      break;
+    case RenderPosition.BEFOREEND:
+      parent.append(child);
+      break;
+    case RenderPosition.AFTEREND:
+      parent.after(child);
+      break;
   }
 };
 
