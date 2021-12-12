45c4917fb994caee0d318a90f43540b517f2c45e
vpalex999 <vpalex@mail.ru>
2021-12-12 14:24:32 +0500

    add func remove

--- src/utils/render.js
+++ src/utils/render.js
@@ -34,4 +34,17 @@ const createElement = (templateElement) => {
   return newElement.firstChild;
 };
 
-export { RenderPosition, render, createElement };
+const remove = (component) => {
+  if (component === null) {
+    return;
+  }
+
+  if (!(component instanceof AbstractView)) {
+    throw new Error('Can remove only components');
+  }
+
+  component.element.remove();
+  component.removeElement();
+};
+
+export { RenderPosition, render, createElement, remove };
