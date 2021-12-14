9baac75448e4bef3f9b474a4b6e929a61561479e
vpalex999 <vpalex@mail.ru>
2021-12-14 20:08:39 +0500

    add replace func

--- src/utils/render.js
+++ src/utils/render.js
@@ -47,4 +47,21 @@ const remove = (component) => {
   component.removeElement();
 };
 
-export { RenderPosition, render, createElement, remove };
+const replace = (newElement, oldElement) => {
+  if (newElement === null || oldElement === null) {
+    throw new Error('Can\'t replace unexisting elements');
+  }
+
+  const newChild = newElement instanceof AbstractView ? newElement.element : newElement;
+  const oldChild = oldElement instanceof AbstractView ? oldElement.element : oldElement;
+
+  const parent = oldChild.parentElement;
+
+  if (parent === null) {
+    throw new Error('Parent element doesn\'t exist');
+  }
+
+  parent.replaceChild(newChild, oldChild);
+};
+
+export { RenderPosition, render, createElement, remove, replace };
