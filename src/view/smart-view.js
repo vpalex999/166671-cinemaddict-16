3a1281265274c7090e37edfc1bdfc3bd627011e9
vpalex999 <vpalex@mail.ru>
2021-12-22 22:23:24 +0500

    add class SmartView

---
+++ src/view/smart-view.js
@@ -0,0 +1,37 @@
+import { AbstractView } from './abstract-view';
+
+class SmartView extends AbstractView {
+
+  _data = {};
+
+  restoreHandlers = () => {
+    throw new Error('Absctract method not implemented: restoreHandlers');
+  };
+
+  updateElement = () => {
+    const prevElement = this.element;
+    const parent = prevElement.parentElement;
+    this.removeElement();
+
+    const newElement = this.element;
+
+    parent.replaceChild(newElement, prevElement);
+    this.restoreHandlers();
+  };
+
+  updateData = (update, justDataUpdating) => {
+    if (!update) {
+      return;
+    }
+
+    this._data = {...this._data, ...update};
+
+    if(justDataUpdating){
+      return;
+    }
+
+    this.updateElement();
+  };
+}
+
+export { SmartView };
