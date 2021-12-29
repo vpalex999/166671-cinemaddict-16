4c82800af8493bc7135d197d1004790211e7dce7
vpalex999 <vpalex@mail.ru>
2021-12-29 08:06:38 +0500

    display comments date

--- src/utils/film.js
+++ src/utils/film.js
@@ -1,7 +1,9 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 dayjs.extend(duration);
+dayjs.extend(relativeTime);
 
 export const updateFilm = (films, updatedFilm) =>
   films.map((film) => film.id === updatedFilm.id ? updatedFilm : film);
@@ -13,3 +15,5 @@ export const sortTaskRating = (filmA, filmB) => filmB.rating - filmA.rating;
 export const getFilmDurationFormat = (period) => dayjs.duration(period, 'minutes').format('H[h] mm[min]');
 
 export const getFilmReleaseDateFormat = (date) => dayjs(date).format('DD MMMM YYYY');
+
+export const getCommentDateFormat = (date) => dayjs(date).fromNow();
